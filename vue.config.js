2784e4e19ee1d1707127844978cabe3a18aa7bb6
ZrtILee <133837508+ZrtILee@users.noreply.github.com>
2023-05-17 16:47:53 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: mingyong.g
+ * @Date: 2020-07-03 22:45:49
+ * @LastEditors: mingyong.g
+ * @LastEditTime: 2020-07-03 22:49:24
+ * @Description: Vue配置文件
+ * @FilePath: \my_foods_waterfull\vue.config.js
+ */
+module.exports = {
+  publicPath: "/my_foods_waterfull/"
+};
